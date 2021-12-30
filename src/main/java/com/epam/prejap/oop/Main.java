54ab85b4ee3a7b6b3e77a9d0e0497cd260a4ea26
Bartłomiej Słomiński <slominskibartlomiej@wp.pl>
2021-12-31 00:30:26 +0100

    Main class and dependency for org.json

---
+++ src/main/java/com/epam/prejap/oop/Main.java
@@ -0,0 +1,22 @@
+package com.epam.prejap.oop;
+
+import org.json.CDL;
+import org.json.JSONArray;
+import org.json.JSONTokener;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String json = """
+[ 
+[3],
+[2],
+]""";
+
+        System.out.println(json);
+
+
+    }
+
+}
