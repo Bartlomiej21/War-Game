fb207d0e4a80186d3f0805e916a2ae9d7a47d7f8
Bartłomiej Słomiński <slominskibartlomiej@wp.pl>
2022-01-04 12:16:22 +0100

    Added Player and Players classes representing their real life counterparts

--- src/main/java/com/epam/prejap/oop/war/Player.java
+++ src/main/java/com/epam/prejap/oop/war/Player.java
@@ -1,2 +1,23 @@
-package com.epam.prejap.oop.war;public class Player {
+package com.epam.prejap.oop.war;
+
+public class Player {
+
+    private String name;
+    private String number;
+
+
+    Player(String playerNumber){
+        this.number = playerNumber;
+        this.name = "P"+playerNumber;
+
+    }
+
+    public String toString(){
+        return "Player"+number;
+    }
+
+    public String getNumber(){
+        System.out.println("Will it work?");
+        return "Number" + number;
+    }
 }
--- src/main/java/com/epam/prejap/oop/war/Players.java
+++ src/main/java/com/epam/prejap/oop/war/Players.java
@@ -1,2 +1,94 @@
-package com.epam.prejap.oop.war;public class Players {
+package com.epam.prejap.oop.war;
+
+import java.util.Iterator;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Players implements Iterable<Player> {
+
+    List<Player> players = new ArrayList<>();
+    int numberOfPlayers;   // NUMBER_OF_PLAYERS
+    final int MIN_PLAYERS = 2;
+    final int MAX_PLAYERS = 4;
+
+
+
+    Players(int numberOfPlayers) {
+        checkPlayersNumber(numberOfPlayers);
+        this.numberOfPlayers = numberOfPlayers;
+        createPlayersList();
+
+    }
+
+    Players(){ }
+
+
+    public List<Integer> createPlayersIntList(int nr){
+        List<Integer> playersInt = IntStream.rangeClosed(1, nr).boxed().collect(Collectors.toList());
+        System.out.println("PlayersInt: "+playersInt);
+        return playersInt;
+
+    }
+
+    public void createPlayersList(){
+
+        //List playersInt = IntStream.rangeClosed(1, numberOfPlayers).boxed().collect(Collectors.toList());
+
+        List<String> playersString = new ConvertIntToStringList( createPlayersIntList(numberOfPlayers) ).stringList;
+        //System.out.println("String of players: " + playersString);
+        for (String s: playersString) players.add(new Player(s));
+
+
+        // tryout implementations for iterators
+        ListIterator<Player> iter = players.listIterator();
+
+        while (iter.hasNext()) { //if next element exists
+            System.out.println(players.get(iter.nextIndex()).getNumber());
+
+            //System.out.println( "Bum lyo" +  players.get(j.nextIndex())  );  //Bum lyoPlayer1
+            //System.out.println("c"+players.get(j.nextIndex()));  // cPlayer1
+
+            iter.next(); // advance the pointer
+        }
+
+        /*
+        Iterator<Player> i = iterator();
+
+        while (i.hasNext()) { //if next element exists
+
+            i.forEachRemaining(System.out::println);   // without this infinite loop
+            //i.next(); // advance the pointer
+        }
+        */
+
+    }
+
+
+    void checkPlayersNumber(int number) {
+        if (number < MIN_PLAYERS || number > MAX_PLAYERS) {
+            System.out.println("Bad player count ");
+            try
+            {
+                Thread.sleep(4000);
+            }
+            catch(InterruptedException e)
+            {
+                System.exit(-1);
+            }
+
+        }
+
+    }
+
+
+
+    @Override
+    public Iterator<Player> iterator(){
+        return players.iterator();
+    }
+
+
+
 }
