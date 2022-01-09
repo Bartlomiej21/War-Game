f135d545063f08fbcbef94a99043580718e6abc7
Bartłomiej Słomiński <slominskibartlomiej@wp.pl>
2022-01-09 12:54:22 +0100

    Added GameScreen class which shows how the game runs every turn

--- src/main/java/com/epam/prejap/oop/war/GameScreen.java
+++ src/main/java/com/epam/prejap/oop/war/GameScreen.java
@@ -1,23 +1,21 @@
 package com.epam.prejap.oop.war;
 
 import java.util.List;
-import java.util.LinkedList;
-
-public class GameScreen {
 
 
-    GameScreen (int nrPlayers, int nrOfCards,List<List<Integer>> allCards){
-        StringBuilder sb = new StringBuilder();
-        String line = "WAR, "+nrPlayers+" players, ? deck, "+nrOfCards+" cards\n";
-        sb.append(line);
-        String line2 = "Player1 played: "+String.valueOf(allCards.get(0)).replace("[","").replace("]","")+"\n";
-        sb.append(line2);
-        String line3 = "Player2 played: "+String.valueOf(allCards.get(1)).replace("[","").replace("]","")+"\n";
-        sb.append(line3);
-        String separator = "############";
-        sb.append(separator);
-        System.out.println(sb);
+public class GameScreen implements Screen {
 
+
+    GameScreen(byte nrOfPlayers,short nrOfCards, List playedCards){
+
+        String message;
+        message = String.format("WAR, %d players, ? deck, %d cards",nrOfPlayers,nrOfCards);
+        for (Byte b: Players.playersNames){
+            message = message.concat("\nPlayer"+b+" played: "+playedCards.get(b-1));
+
+        }
+
+        System.out.println(message);
     }
 
 
