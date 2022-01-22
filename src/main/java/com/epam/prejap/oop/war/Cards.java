9d9b7968e8f4f6bedb45af7dced8d4119b0618c4
Bartłomiej Słomiński <slominskibartlomiej@wp.pl>
2022-01-22 15:45:41 +0100

    Adding Cards class to deal with list of cards operations

--- src/main/java/com/epam/prejap/oop/war/Cards.java
+++ src/main/java/com/epam/prejap/oop/war/Cards.java
@@ -1,2 +1,41 @@
-package com.epam.prejap.oop.war;public class Cards {
+package com.epam.prejap.oop.war;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+
+public class Cards implements Iterable<Card> {
+
+    private List<Card> cards;
+
+    Cards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    Cards() {
+        this(new ArrayList<>());
+    }
+
+    @Override
+    public Iterator<Card> iterator() {
+        return cards.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Card> action) {
+        cards.forEach(action);
+    }
+
+    @Override
+    public Spliterator<Card> spliterator() {
+        return cards.spliterator();
+    }
+
+    public List<Card> getCards(){
+        return cards;
+    }
+
+
+
+
 }
