6fe19e344b5d620c243595b99611980cd2750b59
Bartłomiej Słomiński <slominskibartlomiej@wp.pl>
2022-01-23 23:39:38 +0100

    JSONParser now deals with preparing cards for players, so the JSON array object does not need to be passed to a different class

--- src/main/java/com/epam/prejap/oop/war/JSONParser.java
+++ src/main/java/com/epam/prejap/oop/war/JSONParser.java
@@ -7,25 +7,62 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
 public class JSONParser {
 
     String json;
+    short totalNrOfCards;
 
     void prepareGame() {
         this.json = loadGameFile();
         if (checkJSONArray(json)) {
             JSONArray ja = new JSONArray(json);
-            Players players = new Players((byte)ja.length());
-            PlayersCards cards = new PlayersCards(ja);
-            if (cards.checkIfNotEmpty(cards.cards)) {
-                RunGame runGame = new RunGame(players, cards.cards,cards.totalNrOfCards); //todo
+            Players players = createPlayers(ja);
+            // i want for the game to run with just players and total nr of cards
+            //Players players = new Players((byte)ja.length());
+
+            //DeckCreator cards = new DeckCreator(ja);
+            //if (cards.checkIfNotEmpty(cards.cards)) {
+            if (true){
+                RunGame runGame = new RunGame(players); //todo
                 runGame.playGame();
             }
         }
     }
+
+    private Players createPlayers(JSONArray ja) {
+
+        Players playersList = new Players();
+        //Players players = new Players(playersList);
+        // todo switch with option of shuffled cards
+
+        for (int i=0; i<ja.length();i++) {
+
+            Matcher matcher = Pattern.compile("\\d+").matcher(String.valueOf(ja.get(i)));
+            Cards playerCards = new Cards();
+
+            while (matcher.find()) {
+                Card card = new Card(Integer.parseInt(matcher.group()));
+                if (card.correctValue) {
+                    playerCards.getCards().add(card);
+                    totalNrOfCards++;
+                }
+            }
+            //playersCards.add(childList);
+            playersList.getPlayers().add(new Player((byte)(i+1),playerCards));
+        }
+        //System.out.println("PLAYERS" + playersList.getPlayers().get(1).getPlayersCards().getCards().get(3).cardValue);
+        return playersList;
+    }
+
+
+
 
     String loadGameFile() {
         ArrayList<String> result = new ArrayList<>();
